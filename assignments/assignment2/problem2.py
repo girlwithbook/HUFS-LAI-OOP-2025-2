fe7c5de9346a6b402be06198efb12f88c975424b
girlwithbook <girlwithbook00@gmail.com>
2025-09-28 18:15:50 +0900

    Update problem2.py

--- assignments/assignment2/problem2.py
+++ assignments/assignment2/problem2.py
@@ -1,3 +1,12 @@
+textops/
+  __init__.py        
+  main.py              
+  clean/
+    __init__.py
+    filters.py        
+  tokenize/
+    __init__.py
+    word.py         
 from textops import clean_text, word_tokens
 s = "  I like python  "
 clean_text(s)
