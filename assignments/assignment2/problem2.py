27d52d52dd7a1fef285a844ff86b14ec5eba1b46
girlwithbook <girlwithbook00@gmail.com>
2025-09-28 18:23:46 +0900

    Update problem2.py

--- assignments/assignment2/problem2.py
+++ assignments/assignment2/problem2.py
@@ -11,5 +11,3 @@ from textops import clean_text, word_tokens
 s = "  I like python  "
 clean_text(s)
 word_tokens("I like python")
-print(clean_text(" \mI lovve python!!  ")) 
-print(word_tokens("I love python"))
